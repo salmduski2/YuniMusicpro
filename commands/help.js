3414b83c8be9c41f5d92f94275cb4cf9c1489f5e
DEV:SAHANDASOAIL <76536335+sahandaso@users.noreply.github.com>
2021-10-26 13:12:27 +0000

    Initial commit

---
+++ commands/help.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+const { support_server } = require("../config.json");
+const { LOCALE } = require("../util/EvobotUtil");
+const i18n = require("i18n");
+
+i18n.setLocale(LOCALE);
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: i18n.__("Musichelp.description"),
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setAuthor(`${message.client.user.username}`, `${message.client.user.displayAvatarURL({ format: "png" })}`)
+      .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
+      .setThumbnail(message.client.user.displayAvatarURL({ format: "png" }))
+      .setDescription(i18n.__("Musichelp.embedDescription"))
+      .setColor("#f2ee77")
+      .setFooter(`Requested by ${message.author.username}`);
+
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `\`\`\`${message.client.prefix}${cmd.name}\`\`\``,
+        `${cmd.description} | Aliases: (${cmd.aliases ? `${cmd.aliases}` : ""})`,
+        true
+      );
+    });
+    helpEmbed.addField(`**Links!**`, `**[Support Server](${support_server || "https://discord.gg/PPAXsxEy3U"}) • [Invite](https://discord.com/oauth2/authorize?client_id=829542348085526569&permissions=140660178879&scope=bott)**`)
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
